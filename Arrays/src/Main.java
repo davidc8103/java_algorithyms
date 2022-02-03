98f718ba7c3db087017b334d981042879be09d9b
DC <DC@DESKTOP-5U87PSB>
2022-02-03 03:39:07 -0500

    test

--- Arrays/src/Main.java
+++ Arrays/src/Main.java
@@ -9,9 +9,9 @@ public class Main {
 		intArray[1] = 35;
 		intArray[2] = -15;
 		intArray[3] = 7;
-        intArray[4] = 55;
-        intArray[5] = 1;
-        intArray[6] = -22;
+		intArray[4] = 55;
+		intArray[5] = 1;
+		intArray[6] = -22;
         
         int index = 0;
         for(int i = 0; i < intArray.length; i++) {
